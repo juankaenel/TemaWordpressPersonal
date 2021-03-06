93275f9d69d2e36b1d17404fb5f16f9fd3e3f0ae
Juan Kaenel <kaenel10@gmail.com>
2021-03-06 17:53:21 -0300

    :art: Template para el autor

---
+++ author.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<!-- Template para el autor -->
+    <main class="pagina seccion no-sidebars contenedor">
+        <?php $autor = get_queried_object();?>
+        <h2 class="text-center texto-primario"> 
+                Autor: <?php echo $autor->data->display_name; ?>
+        </h2>
+
+        <p class="text-center">
+            <?php echo get_the_author_meta('description',$autor->data->ID);?>
+        </p>
+
+        <ul class="lista-blog">
+           <?php get_template_part('template-parts/loop','blog'); ?>
+        </ul>
+    </main>
+
+<?php get_footer(); ?>

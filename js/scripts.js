b638f45bafea5f8d8892e66ee114ceff0370b253
Juan Kaenel <kaenel10@gmail.com>
2021-03-11 16:18:28 -0300

    :art: barra nav movible para ordenadores

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,20 @@ jQuery(document).ready( $ =>{
     }
     );
 });
+
+// Agrega posición fija en el header al hacer scroll
+window.onscroll = () => {
+    const scroll = window.scrollY;
+    
+    const headerNav = document.querySelector('.barra-navegacion');
+    const documentBody = document.querySelector('body');
+    // si la cantidad de scroll es mayor a, agregar una clase
+    if(scroll > 300){
+        headerNav.classList.add('fixed-top');
+        documentBody.classList.add('ft-activo');
+    }
+    else{
+        documentBody.classList.remove('ft-activo');
+        headerNav.classList.remove('fixed-top');
+    }
+}
